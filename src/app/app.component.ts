180f7b0259486e5d5450a1a0d51d80eaffeb424b
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2019-06-11 15:11:13 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  socket;
   title = 'app';
+
+  constructor() {
+    this.connectSocket();
+  }
+  connectSocket() {
+    console.log('inside the socket');
+    const socketUrl = 'http://my_ip:5000';
+    this.socket = io(socketUrl);
+    console.log(this.socket);
+    this.socket.on('connect', () => {
+      console.log('socket connected!');
+    });
+
+    this.socket.on('SocketFromNode', (data) => {
+      console.log('Get data is', data);
+      this.socket.emit('DataSuccessful');
+    });
+
+    this.socket.on('disconnect',  () => {
+    });
+  }
 }
